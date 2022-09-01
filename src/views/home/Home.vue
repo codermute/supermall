0936ebee7e27f8a68464cfffa3eb275561e5f40f
dai <8526238+dai25572236638@user.noreply.gitee.com>
2022-09-01 11:58:54 +0800

    scroll

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,117 +1,151 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    
-      <home-swiper :banners="banners"/>
-    <recommend-view :recommends="recommends"/>
-    <feature-view/>
-    <tab-control class="tab-control" 
-    :titles="['流行', '精选',  '新款']" @tabClick="tabClick"/>
-    <input type="text">
-    <good-list :goods="showGoods"/>
-    
-    
-    <back-top @click.native="backClick"/>
-    
+
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+    <feature-view />
+    <tab-control
+      class="tab-control"
+      :titles="['流行', '精选', '新款']"
+      @tabClick="tabClick"
+    />
+    <div ref="reference">
+      <good-list :goods="showGoods" />
+    </div>
+
+    <back-top @click.native="backClick" />
   </div>
 </template>
 
 <script>
-import NavBar from '@/components/common/navbar/NavBar'
-import TabControl from '@/components/content/tabControl/TabControl'
-import GoodList from '@/components/content/goods/GoodsList'
-import BackTop from '@/components/content/backTop/BackTop'
+import NavBar from "@/components/common/navbar/NavBar";
+import TabControl from "@/components/content/tabControl/TabControl";
+import GoodList from "@/components/content/goods/GoodsList";
+import BackTop from "@/components/content/backTop/BackTop";
 
-import HomeSwiper from './childComps/HomeSwiper'
-import RecommendView from './childComps/RecommendView'
-import FeatureView from './childComps/FeatureView'
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
 
-import  {getHomeMultidata, getHomeGoods } from '@/network/home'
+import { getHomeMultidata, getHomeGoods } from "@/network/home";
 
 export default {
-name: 'fdfdsf',
-components: {
-  NavBar,
-  TabControl,
-  GoodList,
-  BackTop,
+  name: "home",
+  components: {
+    NavBar,
+    TabControl,
+    GoodList,
+    BackTop,
 
-  HomeSwiper,
-  FeatureView,
-  RecommendView,
-
-
-},
+    HomeSwiper,
+    FeatureView,
+    RecommendView
+  },
   data() {
     return {
       banners: [],
       recommends: [],
       goods: {
-        'pop': {page: 0, list: []},
-        'new': {page: 0, list: []},
-        'sell': {page: 0, list: []},
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
       },
-      currentType: 'pop'
-    }
-    
+      currentType: "pop"
+    };
   },
   computed: {
     showGoods() {
-      return this.goods[this.currentType].list
+      return this.goods[this.currentType].list;
     }
-    },
+  },
   created() {
-    this.getHomeMultidata()
+    this.getHomeMultidata();
 
-    this.getHomeGoods('pop')
-    this.getHomeGoods('new')
-    this.getHomeGoods('sell')
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+
+    window.addEventListener("scroll", this.Scrollbottom);
+  },
+  mounted() {
+    // this.$refs.rightDiv.addEventListener("scroll", this.scroll, true);
+    // console.log(this.$refs.rightDiv.scrollHeight);
+    // window.addEventListener("scroll", () => {
+    //   let top =
+    //     document.documentElement.scrollTop ||
+    //     document.body.scrollTop ||
+    //     window.pageYOffset;
+    //   let clientHeight = document.documentElement.clientHeight;
+    //   let scrollHeight = document.documentElement.scrollHeight;
+    //   console.log(top, clientHeight, scrollHeight);
+    //   if (top + clientHeight >= scrollHeight) {
+    //     this.getHomeGoods(this.currentType);
+    //   }
+    // if (top > 700) {
+    //   this.scrollTag = true;
+    // } else {
+    //   this.scrollTag = false;
+    // }
+    // });
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.Scrollbottom);
   },
   methods: {
     tabClick(index) {
-      switch(index) {
+      switch (index) {
         case 0:
-        this.currentType = 'pop'
-        break
+          this.currentType = "pop";
+          break;
         case 1:
-        this.currentType = 'new'
-        break
+          this.currentType = "new";
+          break;
         case 2:
-        this.currentType = 'sell'
-        break
+          this.currentType = "sell";
+          break;
       }
     },
     backClick() {
-      this.$refs.Scroll.ScrollTo(0 ,0)
+      this.$refs.Scroll.ScrollTo(0, 0);
     },
 
     getHomeMultidata() {
       getHomeMultidata().then(res => {
-      // this.result = res;
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    })
+        // this.result = res;
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
     },
-    
-  
-  getHomeGoods(type) {
-    const page = this.goods[type].page + 1
-    getHomeGoods(type, page).then(res => {
-      this.goods[type].list.push(...res.data.list)
-      this.goods[type].page += 1
-    })
+
+    getHomeGoods(type) {
+      const page = this.goods[type]?.page + 1;
+      getHomeGoods(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      });
+    },
+    Scrollbottom() {
+      let scrollTop =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      let clientHeight = document.documentElement.clientHeight;
+      let scrollHeight = document.documentElement.scrollHeight;
+      // console.log(scrollTop, clientHeight, scrollHeight);
+      if (scrollTop + clientHeight >= scrollHeight) {
+        console.log("滚动到底部了");
+        // this.getHomeGoods(this.currentType);
+      }
+    }
   }
-  },
-  
-}
+};
 </script>
 
 <style>
-#home{
+#home {
   padding-top: 44px;
 }
 
-.home-nav{
+.home-nav {
   background-color: var(--color-tint);
   color: #fff;
 
--- vue.config.js
+++ vue.config.js
@@ -10,28 +10,28 @@ module.exports = {
             }
         }
     },
-    css: {
-        loaderOptions: {
-          postcss: {
-            plugins: [
-              require("postcss-px-to-viewport")({
-                unitToConvert: "px",
-                viewportWidth: 750,
-                viewportHeight: 667,
-                unitPrecision: 3,
-                propList: [
-                  "*"
-                ],
-                viewportUnit: "vw",
-                fontViewportUnit: "vw",
-                selectorBlackList: ['.ignore', '.hairlines'],
-                minPixelValue: 1,
-                mediaQuery: false,
-                replace: true,
-                exclude: /(\/|\\)(node_modules)(\/|\\)/,
-              })
-            ]
-          }
-        }
-      }
+    // css: {
+    //     loaderOptions: {
+    //       postcss: {
+    //         plugins: [
+    //           require("postcss-px-to-viewport")({
+    //             unitToConvert: "px",
+    //             viewportWidth: 750,
+    //             viewportHeight: 667,
+    //             unitPrecision: 3,
+    //             propList: [
+    //               "*"
+    //             ],
+    //             viewportUnit: "vw",
+    //             fontViewportUnit: "vw",
+    //             selectorBlackList: ['.ignore', '.hairlines'],
+    //             minPixelValue: 1,
+    //             mediaQuery: false,
+    //             replace: true,
+    //             exclude: /(\/|\\)(node_modules)(\/|\\)/,
+    //           })
+    //         ]
+    //       }
+    //     }
+    //   }
 }
